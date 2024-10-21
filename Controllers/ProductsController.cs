e438a0c6117f9eee4e9c04561d80b3bd1bed48a8
KenChien <andisand@gmail.com>
2024-10-21 14:43:06 +0800

    GetProducts controller

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,11 +1,22 @@
+using lab1_webapi_event.Entity;
+using lab1_webapi_event.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lab1_webapi_event.Controllers;
 
-[Route("api/[products]")]
+[Route("api/products")]
 [ApiController]
 public class ProductsController(IMediator mediator) : ControllerBase
 {
    private readonly IMediator _mediator = mediator;
+
+
+   [HttpGet]
+   public async Task<IActionResult> GetProducts()
+   {
+      IEnumerable<Product> products = await _mediator.Send(new GetProductQuery());
+
+      return Ok(products);
+   }
 }
---
+++ Handler/GetProductHandler.cs
@@ -0,0 +1,19 @@
+
+
+using lab1_webapi_event.Entity;
+using lab1_webapi_event.Queries;
+using lab1_webapi_event.Store;
+using MediatR;
+
+namespace lab1_webapi_event.Handler
+{
+    public class GetProductHandler(MockDataStore mockDataStore) : IRequestHandler<GetProductQuery, IEnumerable<Product>>
+    {
+        private readonly MockDataStore _mockDataStore  = mockDataStore;
+
+        public async Task<IEnumerable<Product>> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        {
+            return await _mockDataStore.GetProducts();
+        }
+    }
+}
