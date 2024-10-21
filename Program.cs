8d71cb9cd7e609dcf7872ebec9f4b1d5dd118a08
KenChien <andisand@gmail.com>
2024-10-21 11:36:04 +0800

    add mediatR

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddMediatR(config=>config.RegisterServicesFromAssemblies(typeof(Program).Assembly));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
